1c56bbf10b978edbb22ba45b3c32c99aee75e56d
Harshrajsinh Chavda <chavdaharshrajsinh8@gmail.com>
2021-12-13 10:44:34 +0530

    Remove default test case and added new test case

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,13 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
-  it("should do something", function() {
-    update_quality();
+  describe("#qualityNextDay", function () {
+    describe("given the item is not brie, sulfuras, backstage pass or conjured", function () {
+      describe("given the item is within sell-by-date", function () {
+        it("should by one less than current quality", function () {
+          var testItem = new Item("mango", 3, 3);
+          expect(testItem.qualityNextDay()).toEqual(2);
+        });
+      });
+    });
   });
-
 });
