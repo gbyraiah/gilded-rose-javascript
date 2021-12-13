8fd3a119b503d6007361f72327a0509fc1caa3dd
Harshrajsinh Chavda <chavdaharshrajsinh8@gmail.com>
2021-12-13 15:13:44 +0530

    added spec for default item

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,8 +1,16 @@
 describe("Gilded Rose", function () {
   describe("#qualityNextDay", function () {
     describe('given that the sellIn value is less than 0', function () {
+      it('increases quality value by 2', function () {
+        let item = new DefaultItem('mango', 3, 0);
+        item.quality = 3;
+        item.sellIn = -1;
+        expect(item.qualityNextDay()).toEqual(2);
+      });
+    });
+    describe('given that the sellIn value is less than 0', function () {
       it('sets quality value to 0', function () {
-        var testPass = new BackStagePass('pass', -1, 20);
+        let testPass = new BackStagePass('pass', -1, 20);
         expect(testPass.qualityNextDay()).toEqual(0);
       });
     });
@@ -46,11 +54,11 @@ describe("Gilded Rose", function () {
     });
     describe("given AgedBrie", function () {
       it("should increase quality value", function () {
-        var item = new AgedBrie("brie", 10, 10);
+        let item = new AgedBrie("brie", 10, 10);
         expect(item.qualityNextDay()).toEqual(11);
       });
       it("should not increase quality value above 50", function () {
-        var item = new AgedBrie("brie", 10, 50);
+        let item = new AgedBrie("brie", 10, 50);
         expect(item.qualityNextDay()).toEqual(50);
       });
     });
@@ -74,19 +82,19 @@ describe("Gilded Rose", function () {
     describe("given BackStagePass", function () {
       describe("given 10 or fewer days until sell-by date", function () {
         it("should increase by 2", function () {
-          var testItem = new BackStagePass("pass", 10, 20);
+          let testItem = new BackStagePass("pass", 10, 20);
           expect(testItem.qualityNextDay()).toEqual(22);
         });
       });
       describe("given 5 or fewer days until sell-by date", function () {
         it("should increase by 3", function () {
-          var testItem = new BackStagePass("pass", 5, 20);
+          let testItem = new BackStagePass("pass", 5, 20);
           expect(testItem.qualityNextDay()).toEqual(23);
         });
       });
       describe("given that concert has happened", function () {
         it("should be zero", function () {
-          var testItem = new BackStagePass("pass", 0, 20);
+          let testItem = new BackStagePass("pass", 0, 20);
           expect(testItem.qualityNextDay()).toEqual(0);
         });
       });
@@ -94,77 +102,77 @@ describe("Gilded Rose", function () {
     describe("given the item is not brie, sulfuras, backstage pass or conjured", function () {
       describe("given the item is within the sell-by-date", function () {
         it("should by one less than current quality", function () {
-          var item = new Item("mango", 3, 3);
+          let item = new Item("mango", 3, 3);
           expect(item.qualityNextDay()).toEqual(2);
           expect(item.qualityNextDay()).toEqual(2);
         });
       });
     });
     it("should never decrease past zero", function () {
-      var item = new Item("mango", 3, 0);
+      let item = new Item("mango", 3, 0);
       expect(item.qualityNextDay()).toEqual(0);
     });
     it("should never be higher than 50", function () {
-      var item = new AgedBrie("brie", 3, 50);
+      let item = new AgedBrie("brie", 3, 50);
       expect(item.qualityNextDay()).toEqual(50);
     });
 
     describe("given the item is past the sell-by-date", function () {
       it("should by two less than current quality", function () {
-        var item = new Item("mango", 0, 3);
+        let item = new Item("mango", 0, 3);
         expect(item.qualityNextDay()).toEqual(1);
       });
     });
     describe("given aged brie", function () {
       it("should be one more than current quality", function () {
-        var item = new AgedBrie("brie", 1, 1);
+        let item = new AgedBrie("brie", 1, 1);
         expect(item.qualityNextDay()).toEqual(2);
       });
     });
     describe("given it is passed expiry", function () {
       it("should be equal to 3", function () {
-        var item = new AgedBrie("brie", 0, 1);
+        let item = new AgedBrie("brie", 0, 1);
         expect(item.qualityNextDay()).toEqual(3);
       });
     });
     describe("given it is passed expiry", function () {
       it("should increase quality value by 2", function () {
-        var item = new AgedBrie("brie", 0, 20);
+        let item = new AgedBrie("brie", 0, 20);
         expect(item.qualityNextDay()).toEqual(22);
       });
       it("should be two more than current quality", function () {
-        var item = new AgedBrie("brie", 0, 1);
+        let item = new AgedBrie("brie", 0, 1);
         expect(item.qualityNextDay()).toEqual(3);
       });
     });
     describe("given sulfuras as item", function () {
       it("should value of quality be 50", function () {
-        var item = new Sulfuras("sulfuras", 1, 50);
+        let item = new Sulfuras("sulfuras", 1, 50);
         expect(item.qualityNextDay()).toEqual(50);
       });
     });
     describe("given BackStagePass", function () {
       describe("given it is has more than 10 days to expire", function () {
         it("should increase by 1", function () {
-          var testItem = new BackStagePass("pass", 11, 20);
+          let testItem = new BackStagePass("pass", 11, 20);
           expect(testItem.qualityNextDay()).toEqual(21);
         });
       });
       describe("given 10 or fewer days until sell-by date", function () {
         it("should increase by 2", function () {
-          var testItem = new BackStagePass("pass", 10, 20);
+          let testItem = new BackStagePass("pass", 10, 20);
           expect(testItem.qualityNextDay()).toEqual(22);
         });
       });
       describe("given 5 or fewer days until sell-by date", function () {
         it("should increase by 3", function () {
-          var testItem = new BackStagePass("pass", 5, 20);
+          let testItem = new BackStagePass("pass", 5, 20);
           expect(testItem.qualityNextDay()).toEqual(23);
         });
       });
       describe("given that the item has expired", function () {
         it("should be zero", function () {
-          var item = new BackStagePass("pass", 0, 20);
+          let item = new BackStagePass("pass", 0, 20);
           expect(item.qualityNextDay()).toEqual(0);
         });
       });
