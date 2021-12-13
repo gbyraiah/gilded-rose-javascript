12b02448c3abf9b23c54612f93997a67900f10d4
Harshrajsinh Chavda <chavdaharshrajsinh8@gmail.com>
2021-12-13 10:55:07 +0530

    Calculate dep based on sell in value

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,17 +2,23 @@ describe("Gilded Rose", function () {
 
   describe("#qualityNextDay", function () {
     describe("given the item is not brie, sulfuras, backstage pass or conjured", function () {
-      describe("given the item is within sell-by-date", function () {
+      describe("given the item is within the sell-by-date", function () {
         it("should by one less than current quality", function () {
-          var testItem = new Item("mango", 3, 3);
-          expect(testItem.qualityNextDay()).toEqual(2);
-          expect(testItem.qualityNextDay()).toEqual(2);
+          var item = new Item("mango", 3, 3);
+          expect(item.qualityNextDay()).toEqual(2);
+          expect(item.qualityNextDay()).toEqual(2);
         });
       });
     });
     it("should never decrease past zero", function () {
-      var testItem = new Item('mango', 3, 0);
-      expect(testItem.qualityNextDay()).toEqual(0);
-    })
+      var item = new Item('mango', 3, 0);
+      expect(item.qualityNextDay()).toEqual(0);
+    });
+    describe("given the item is past the sell-by-date", function () {
+      it("should by two less than current quality", function () {
+        var item = new Item('apple', 0, 3);
+        expect(item.qualityNextDay()).toEqual(1);
+      });
+    });
   });
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,33 +1,42 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 
-  qualityNextDay () {
-    let qualityNextDay = this.quality - 1;
+  qualityNextDay() {
+    let qualityNextDay = this.quality - this.calculateDepValue();
     if (qualityNextDay < 0) return 0;
     return qualityNextDay;
+  }
+
+  calculateDepValue() {
+    return this.sellIn === 0 ? 2 : 1;
   }
 }
 
 class GildedRose {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   update_quality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        this.items[i].name != "Aged Brie" &&
+        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+      ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+          ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -41,19 +50,22 @@ class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != "Aged Brie") {
+          if (
+            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+          ) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
