6e4c2168da173c08a5cc69c7f08f61edc2220b99
Harshrajsinh Chavda <chavdaharshrajsinh8@gmail.com>
2021-12-13 13:00:11 +0530

    removed static logic

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -66,11 +66,7 @@ class GildedRose {
           if (
             this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
           ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].qualityNextDay();
-              }
-            }
+            this.items[i].quality = this.items[i].qualityNextDay();
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].qualityNextDay();
